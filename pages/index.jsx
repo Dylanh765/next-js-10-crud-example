06f6f057c1d2b6d6a2759ea399a2c874463bf791
Dylan Hughes <47777699+Dylanh765@users.noreply.github.com>
2021-06-24 08:40:40 +0100

    Add logic for displaying different data between pages

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'components';
+import Header from '../components/header'
 
 export default Home;
 
 function Home() {
     return (
         <div>
+        <Header />
             <h1>Next.js 10 - CRUD Example with React Hook Form</h1>
             <p>An example app showing how to list, add, edit and delete user records with Next.js 10 and the React Hook Form library.</p>
             <p><Link href="/users">&gt;&gt; Manage Users</Link></p>
